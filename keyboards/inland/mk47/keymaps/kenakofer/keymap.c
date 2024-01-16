4d436095fb1c61770e8e66b29bad4856585cbb90
Kenan Schaefkofer <kbitikofer@gmail.com>
2024-01-15 21:26:01 -0800

    Add GUI layer in qwerty style

--- keyboards/inland/mk47/keymaps/kenakofer/keymap.c
+++ keyboards/inland/mk47/keymaps/kenakofer/keymap.c
@@ -89,7 +89,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         } else if (keycode != KC_LCTL && keycode != KC_LSFT && keycode != KC_LALT && keycode != KC_LGUI
                     && keycode != KC_RCTL && keycode != KC_RSFT && keycode != KC_RALT && keycode != KC_RGUI
                     // Also ignore the layer keys
-                    && keycode != MO(2) && keycode != MO(3) && keycode != MO(4) && keycode != MO(5)
+                    && keycode != MO(2) && keycode != MO(3) && keycode != MO(4) && keycode != MO(5) && keycode != MO(6)
                     // and arrow keys
                     && keycode != KC_UP && keycode != KC_DOWN && keycode != KC_LEFT && keycode != KC_RGHT
                     ) {
@@ -153,12 +153,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_ESC, KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_QUOT, KC_BSPC,
                 MT(MOD_RCTL, KC_TAB), KC_A, KC_R, KC_S, KC_T, KC_G, KC_M, KC_N, KC_E, KC_I, KC_O, KC_ENT,
                 KC_Z, KC_LSFT, KC_X, KC_C, KC_D, KC_V, KC_K, KC_H, KC_COMM, KC_DOT, KC_UP, MT(MOD_RSFT, KC_SLSH),
-                KC_LCTL, KC_LALT, MO(5), KC_LGUI, MO(2), KC_SPC, MO(3), MO(4), KC_LEFT, KC_DOWN, KC_RGHT),
+                KC_LCTL, KC_LALT, MO(6), LM(4, MOD_LGUI), MO(2), KC_SPC, MO(3), MO(5), KC_LEFT, KC_DOWN, KC_RGHT),
 	[1] = LAYOUT_planck_mit(
                 KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
-                MT(MOD_RCTL, KC_TAB), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
+                KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
                 KC_Z, KC_LSFT, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_UP, MT(MOD_RSFT, KC_SLSH),
-                KC_LCTL, KC_LALT, MO(5), KC_LGUI, MO(2), KC_SPC, MO(3), MO(4), KC_LEFT, KC_DOWN, KC_RGHT),
+                KC_LCTL, KC_LALT, MO(6), LM(4, MOD_LGUI), MO(2), KC_SPC, MO(3), MO(5), KC_LEFT, KC_DOWN, KC_RGHT),
 	[2] = LAYOUT_planck_mit(
                 KC_GRV, RSFT(KC_1), RSFT(KC_2), RSFT(KC_3), RSFT(KC_4), RSFT(KC_5), KC_BSLS, KC_EQL, KC_LBRC, KC_RBRC, KC_QUOT, KC_TRNS,
                 KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS,
@@ -170,14 +170,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_TRNS, RSFT(KC_6), RSFT(KC_7), RSFT(KC_8), RSFT(KC_9), RSFT(KC_0), KC_NO, KC_MINS, RSFT(KC_COMM), RSFT(KC_DOT), KC_SCLN, RSFT(KC_SLSH),
                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 	[4] = LAYOUT_planck_mit(
-                KC_TRNS, KC_HOME, KC_PGUP, KC_PGDN, KC_END, KC_NO, KC_NO, RCS(KC_TAB), RALT(KC_LEFT), RALT(KC_RGHT), RCTL(KC_TAB), KC_DEL,
-                AUTO_TAB, AUTO_LEFT, AUTO_UP, AUTO_DOWN, AUTO_RGHT, KC_NO, KC_WH_U, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_TRNS,
-                RCTL(KC_Z), KC_TRNS, RCTL(KC_X), RCTL(KC_C), RCTL(KC_V), RCTL(KC_V), KC_WH_D, RCTL(KC_LEFT), RCTL(KC_UP), RCTL(KC_DOWN), RCTL(KC_RGHT), KC_RSFT,
-                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RCTL(KC_MINS), RCTL(KC_EQL), RCS(KC_EQL)),
+                KC_GRV, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
+                KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
+                KC_Z, KC_LSFT, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_UP, MT(MOD_RSFT, KC_SLSH),
+                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SPC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 	[5] = LAYOUT_planck_mit(
+                KC_TRNS, KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R, KC_PGUP, KC_NO, RCS(KC_TAB), RALT(KC_LEFT), RALT(KC_RGHT), RCTL(KC_TAB), KC_DEL,
+                AUTO_TAB, AUTO_LEFT, AUTO_UP, AUTO_DOWN, AUTO_RGHT, KC_PGDN, KC_NO, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_TRNS,
+                RCTL(KC_Z), KC_LSFT, RCTL(KC_X), RCTL(KC_C), RCTL(KC_V), KC_HOME, KC_END, RCTL(KC_LEFT), RCTL(KC_UP), RCTL(KC_DOWN), RCTL(KC_RGHT), KC_RSFT,
+                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RCTL(KC_MINS), RCTL(KC_EQL), RCS(KC_EQL)),
+	[6] = LAYOUT_planck_mit(
                 KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, ECHO_BACKS,
                 KC_CAPS, KC_BRID, KC_VOLU, KC_VOLD, KC_BRIU, RGB_MOD, RGB_RMOD, KC_NO, KC_NO, KC_F11, KC_F12, ECHO_CHARS,
-                KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_VAI, KC_RSFT,
+                KC_NO, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_VAI, KC_RSFT,
                 KC_NO, KC_MUTE, KC_TRNS, KC_NO, KC_NO, KC_MPLY, KC_NO, KC_NO, DF(1), RGB_VAD, DF(0))
 };
 
@@ -230,9 +235,12 @@ layer_state_t layer_state_set_user(layer_state_t state) {
         set_hue (0xAA);
         break;
     case 4:
-        set_hue (0xCC);
+        set_hue (0x00);
         break;
     case 5:
+        set_hue (0xCC);
+        break;
+    case 6:
         set_hue (0x44);
         break;
     default: //  for any other layers, or the default layer
